31de8d231e5becfe48b3396255d146f2bad8a19a
Kabir Bose <kabirbose04@gmail.com>
2025-03-23 19:38:54 -0400

    updated connection form

--- src/app/components/ui/ConnectionInfo.tsx
+++ src/app/components/ui/ConnectionInfo.tsx
@@ -6,9 +6,9 @@ export default function ConnectionInfo() {
       <p className="font-bold">Database Connection</p>
       <form className="flex flex-col gap-2">
         <ConnectionInfoField text="Hostname" />
-        <ConnectionInfoField text="Port ID" />
+        <ConnectionInfoField text="Port ID" type="number" />
         <ConnectionInfoField text="Username" />
-        <ConnectionInfoField text="Password" />
+        <ConnectionInfoField text="Password" type="password" />
         <ConnectionInfoField text="Database" />
       </form>
     </div>
--- src/app/components/ui/ConnectionInfoField.tsx
+++ src/app/components/ui/ConnectionInfoField.tsx
@@ -1,12 +1,16 @@
 interface Props {
   text: string;
+  type?: string;
 }
 
-export default function ConnectionInfoField({ text }: Props) {
+export default function ConnectionInfoField({ text, type }: Props) {
   return (
     <div className="w-[18rem] flex justify-between items-center">
       <label>{text}: </label>
-      <input className="bg-zinc-800 outline-0 border-0 rounded-md p-1" />
+      <input
+        type={type || "text"}
+        className="bg-zinc-800 outline-0 border-0 rounded-md p-1"
+      />
     </div>
   );
 }
