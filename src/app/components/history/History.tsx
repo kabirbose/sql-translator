a4004c510feed6957a3f7d04af08a24df239dd98
Kabir Bose <kabirbose04@gmail.com>
2025-03-30 16:24:53 -0400

    removed scroll bar

--- src/app/components/history/History.tsx
+++ src/app/components/history/History.tsx
@@ -7,7 +7,7 @@ export default function History() {
   const { historyBlock } = useHistoryContext();
 
   return (
-    <div className="h-[60rem] p-5 flex flex-col gap-2 overflow-y-scroll">
+    <div className="h-[25rem] p-5 flex flex-col gap-2 overflow-y-scroll [&::-webkit-scrollbar]:hidden">
       <p className="font-bold">History</p>
       {historyBlock.map((block) => {
         return (
--- src/app/components/history/HistoryItem.tsx
+++ src/app/components/history/HistoryItem.tsx
@@ -3,26 +3,11 @@ interface Props {
 }
 
 export default function HistoryItem({ text }: Props) {
-  var currentdate = new Date();
-  var datetime =
-    currentdate.getDate() +
-    "/" +
-    (currentdate.getMonth() + 1) +
-    "/" +
-    currentdate.getFullYear() +
-    ", " +
-    currentdate.getHours() +
-    ":" +
-    currentdate.getMinutes() +
-    ":" +
-    currentdate.getSeconds();
-
   return (
     <div className="border-b-2 p-2 border-zinc-800 flex flex-col gap-2">
       <p className="bg-zinc-800 text-purple-400 p-1 rounded-md font-mono">
         {text}
       </p>
-      <p>{datetime}</p>
     </div>
   );
 }
